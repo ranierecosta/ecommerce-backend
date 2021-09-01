acd1c20a54e2dec00e4c51130bb19c706822ff30
raniere costa <raniere.costa@totvs.com.br>
2021-09-01 07:58:42 -0300

    first commit

---
+++ Infrastructure/Services/PaymentService.cs
@@ -0,0 +1,117 @@
+﻿using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.Extensions.Configuration;
+using Stripe;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Order = Core.Entities.OrderAggregate.Order;
+using Product = Core.Entities.Product;
+
+namespace Infrastructure.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _config;
+
+        public PaymentService(IBasketRepository basketRepository, IUnitOfWork unitOfWork, IConfiguration config)
+        {
+            _basketRepository = basketRepository;
+            _unitOfWork = unitOfWork;
+            _config = config;
+        }
+
+        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            StripeConfiguration.ApiKey = _config["StripeSettings:SecretKey"];
+
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+
+            if (basket == null) return null;
+
+            var shippingPrice = 0m;
+
+            if (basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync((int)basket.DeliveryMethodId);
+                shippingPrice = deliveryMethod.Price;
+            }
+
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+
+                if (item.Price != productItem.Price)
+                {
+                    item.Price = productItem.Price;
+                }
+            }
+
+            var paymentService = new PaymentIntentService();
+
+            PaymentIntent paymentIntent;
+
+            if (string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var options = new PaymentIntentCreateOptions
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string> { "card" }
+                };
+
+                paymentIntent = await paymentService.CreateAsync(options);
+
+                basket.PaymentIntentId = paymentIntent.Id;
+                basket.ClientSecret = paymentIntent.ClientSecret;
+            }
+            else
+            {
+                var options = new PaymentIntentUpdateOptions
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100
+                };
+
+                await paymentService.UpdateAsync(basket.PaymentIntentId, options);
+            }
+
+            await _basketRepository.UpdateBasketAsync(basket);
+
+            return basket;
+        }
+
+        public async Task<Order> UpdateOrderPaymentFailed(string paymentIntentId)
+        {
+            var spec = new OrderByPaymentIntentIdSpecification(paymentIntentId);
+            var order = await _unitOfWork.Repository<Order>().GetEntityWithSpec(spec);
+
+            if (order == null) return null;
+
+            order.Status = OrderStatus.PaymentFailed;
+
+            await _unitOfWork.Complete();
+
+            return order;
+        }
+
+        public async Task<Order> UpdateOrderPaymentSucceeded(string paymentIntentId)
+        {
+            var spec = new OrderByPaymentIntentIdSpecification(paymentIntentId);
+            var order = await _unitOfWork.Repository<Order>().GetEntityWithSpec(spec);
+
+            if (order == null) return null;
+
+            order.Status = OrderStatus.PaymentRecevied;
+
+            _unitOfWork.Repository<Order>().Update(order);
+
+            await _unitOfWork.Complete();
+
+            return order;
+        }
+    }
+}
---
+++ UnitTestsAPI/UnitTestExample.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTestsAPI
+{
+    public class UnitTestExample
+    {
+        [Fact]
+        public void TestExample()
+        {
+            Assert.True(true);
+        }
+    }
+}
